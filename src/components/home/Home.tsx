56bd2cdb1894e60ded42512985bf5d5e8651b2db
Victor Muniz <victorrmunizz@gmail.com>
2023-12-05 20:48:53 -0300

    create contact screen

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,33 +1,38 @@
 
 import { Blob } from '../Blob/Blob';
+import { Contact } from '../contact/Contact';
 import { Nav } from '../nav/Nav';
 import styles from './Home.module.scss';
 
 export function Home() {
     return (
-        <div className={styles.homeContainer}>
-            <div className={`${styles.spacer} ${styles.layer1}`}></div>
-            <section className={styles.mainSection}>
-                <div className={styles.info}>
-                    <h1>
-                        Víctor Muniz
-                    </h1>
-                    <h1>
-                        Full Stack Developer
-                    </h1>
-                </div>
-                <div className={styles.blob}>
-                    <Blob />
-                </div>
-                <div className={styles.blob}>
-                    <Blob />
-                </div>
-            </section>
-            <div className={`${styles.spacer} ${styles.layer2}`}></div>
+        <>
+            <div className={styles.homeContainer}>
+                <div className={`${styles.spacer} ${styles.layer1}`}></div>
+                <section className={styles.mainSection}>
+                    <div className={styles.info}>
+                        <h1>
+                            Víctor Muniz
+                        </h1>
+                        <h1>
+                            Full Stack Developer
+                        </h1>
+                    </div>
+                    <div className={styles.blob}>
+                        <Blob />
+                    </div>
+                    <div className={styles.blob}>
+                        <Blob />
+                    </div>
+                </section>
+                <div className={`${styles.spacer} ${styles.layer2}`}></div>
 
-            <section className={styles.nav}>
-                <Nav />
-            </section>
-        </div>
+                <section className={styles.nav}>
+                    <Nav />
+                </section>
+            </div>
+            
+            <Contact />
+        </>
     );
 }

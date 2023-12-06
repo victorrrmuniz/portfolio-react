d0bdecda956ccebddad14bbe7976f419849e7264
Victor Muniz <victorrmunizz@gmail.com>
2023-12-06 20:44:10 -0300

    change home background animation

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,14 +1,35 @@
 
-import { Blob } from '../Blob/Blob';
 import { Contact } from '../contact/Contact';
 import { Nav } from '../nav/Nav';
 import styles from './Home.module.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faCoffee, 
+    faFilm, 
+    faHeadphones, 
+    faBook,
+    faVideo,
+    faGamepad,
+    faGlobe,
+    faComputer,
+    faDesktop,
+    faLightbulb,
+    faPuzzlePiece,
+    faKeyboard,
+    faCodeBranch,
+    faWaterLadder,
+    faVolumeHigh,
+    faUtensils,
+    faUserGraduate,
+    faPodcast,
+    faPizzaSlice
+} from '@fortawesome/free-solid-svg-icons';
+
 export function Home() {
     return (
         <>
-            <div className={styles.homeContainer}>
-                <div className={`${styles.spacer} ${styles.layer1}`}></div>
+            {/* <div className={styles.homeContainer}>
                 <section className={styles.mainSection}>
                     <div className={styles.info}>
                         <h1>
@@ -18,19 +39,194 @@ export function Home() {
                             Full Stack Developer
                         </h1>
                     </div>
-                    <div className={styles.blob}>
-                        <Blob />
-                    </div>
-                    <div className={styles.blob}>
-                        <Blob />
-                    </div>
                 </section>
                 <div className={`${styles.spacer} ${styles.layer2}`}></div>
 
                 <section className={styles.nav}>
                     <Nav />
                 </section>
-            </div>
+            </div> */}
+
+            <section className={styles.iconsSection}>
+                <div className={styles.iconsRow}>
+                    <div>
+                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faFilm} />
+                        <FontAwesomeIcon icon={faHeadphones} />
+                        <FontAwesomeIcon icon={faBook} />
+                        <FontAwesomeIcon icon={faVideo} />
+                        <FontAwesomeIcon icon={faGamepad} />
+                        <FontAwesomeIcon icon={faGlobe} />
+                        <FontAwesomeIcon icon={faComputer} />
+                        <FontAwesomeIcon icon={faDesktop} />
+                        <FontAwesomeIcon icon={faLightbulb} />
+                        <FontAwesomeIcon icon={faPuzzlePiece} />
+                        <FontAwesomeIcon icon={faKeyboard} />
+                        <FontAwesomeIcon icon={faCodeBranch} />
+                        <FontAwesomeIcon icon={faWaterLadder} />
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                        <FontAwesomeIcon icon={faUtensils} />
+                        <FontAwesomeIcon icon={faUserGraduate} />
+                        <FontAwesomeIcon icon={faPodcast} />
+                        <FontAwesomeIcon icon={faPizzaSlice} />
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faFilm} />
+                        <FontAwesomeIcon icon={faHeadphones} />
+                        <FontAwesomeIcon icon={faBook} />
+                        <FontAwesomeIcon icon={faVideo} />
+                        <FontAwesomeIcon icon={faGamepad} />
+                        <FontAwesomeIcon icon={faGlobe} />
+                        <FontAwesomeIcon icon={faComputer} />
+                        <FontAwesomeIcon icon={faDesktop} />
+                        <FontAwesomeIcon icon={faLightbulb} />
+                        <FontAwesomeIcon icon={faPuzzlePiece} />
+                        <FontAwesomeIcon icon={faKeyboard} />
+                        <FontAwesomeIcon icon={faCodeBranch} />
+                        <FontAwesomeIcon icon={faWaterLadder} />
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                        <FontAwesomeIcon icon={faUtensils} />
+                        <FontAwesomeIcon icon={faUserGraduate} />
+                        <FontAwesomeIcon icon={faPodcast} />
+                        <FontAwesomeIcon icon={faPizzaSlice} />
+                    </div>
+                </div>
+
+                <div className={styles.iconsRow}>
+                    <div>
+                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faFilm} />
+                        <FontAwesomeIcon icon={faHeadphones} />
+                        <FontAwesomeIcon icon={faBook} />
+                        <FontAwesomeIcon icon={faVideo} />
+                        <FontAwesomeIcon icon={faGamepad} />
+                        <FontAwesomeIcon icon={faGlobe} />
+                        <FontAwesomeIcon icon={faComputer} />
+                        <FontAwesomeIcon icon={faDesktop} />
+                        <FontAwesomeIcon icon={faLightbulb} />
+                        <FontAwesomeIcon icon={faPuzzlePiece} />
+                        <FontAwesomeIcon icon={faKeyboard} />
+                        <FontAwesomeIcon icon={faCodeBranch} />
+                        <FontAwesomeIcon icon={faWaterLadder} />
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                        <FontAwesomeIcon icon={faUtensils} />
+                        <FontAwesomeIcon icon={faUserGraduate} />
+                        <FontAwesomeIcon icon={faPodcast} />
+                        <FontAwesomeIcon icon={faPizzaSlice} />
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faFilm} />
+                        <FontAwesomeIcon icon={faHeadphones} />
+                        <FontAwesomeIcon icon={faBook} />
+                        <FontAwesomeIcon icon={faVideo} />
+                        <FontAwesomeIcon icon={faGamepad} />
+                        <FontAwesomeIcon icon={faGlobe} />
+                        <FontAwesomeIcon icon={faComputer} />
+                        <FontAwesomeIcon icon={faDesktop} />
+                        <FontAwesomeIcon icon={faLightbulb} />
+                        <FontAwesomeIcon icon={faPuzzlePiece} />
+                        <FontAwesomeIcon icon={faKeyboard} />
+                        <FontAwesomeIcon icon={faCodeBranch} />
+                        <FontAwesomeIcon icon={faWaterLadder} />
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                        <FontAwesomeIcon icon={faUtensils} />
+                        <FontAwesomeIcon icon={faUserGraduate} />
+                        <FontAwesomeIcon icon={faPodcast} />
+                        <FontAwesomeIcon icon={faPizzaSlice} />
+                    </div>
+                </div>
+                <div className={styles.iconsRow}>
+                    <div>
+                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faFilm} />
+                        <FontAwesomeIcon icon={faHeadphones} />
+                        <FontAwesomeIcon icon={faBook} />
+                        <FontAwesomeIcon icon={faVideo} />
+                        <FontAwesomeIcon icon={faGamepad} />
+                        <FontAwesomeIcon icon={faGlobe} />
+                        <FontAwesomeIcon icon={faComputer} />
+                        <FontAwesomeIcon icon={faDesktop} />
+                        <FontAwesomeIcon icon={faLightbulb} />
+                        <FontAwesomeIcon icon={faPuzzlePiece} />
+                        <FontAwesomeIcon icon={faKeyboard} />
+                        <FontAwesomeIcon icon={faCodeBranch} />
+                        <FontAwesomeIcon icon={faWaterLadder} />
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                        <FontAwesomeIcon icon={faUtensils} />
+                        <FontAwesomeIcon icon={faUserGraduate} />
+                        <FontAwesomeIcon icon={faPodcast} />
+                        <FontAwesomeIcon icon={faPizzaSlice} />
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faFilm} />
+                        <FontAwesomeIcon icon={faHeadphones} />
+                        <FontAwesomeIcon icon={faBook} />
+                        <FontAwesomeIcon icon={faVideo} />
+                        <FontAwesomeIcon icon={faGamepad} />
+                        <FontAwesomeIcon icon={faGlobe} />
+                        <FontAwesomeIcon icon={faComputer} />
+                        <FontAwesomeIcon icon={faDesktop} />
+                        <FontAwesomeIcon icon={faLightbulb} />
+                        <FontAwesomeIcon icon={faPuzzlePiece} />
+                        <FontAwesomeIcon icon={faKeyboard} />
+                        <FontAwesomeIcon icon={faCodeBranch} />
+                        <FontAwesomeIcon icon={faWaterLadder} />
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                        <FontAwesomeIcon icon={faUtensils} />
+                        <FontAwesomeIcon icon={faUserGraduate} />
+                        <FontAwesomeIcon icon={faPodcast} />
+                        <FontAwesomeIcon icon={faPizzaSlice} />
+                    </div>
+                </div>
+                <div className={styles.iconsRow}>
+                    <div>
+                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faFilm} />
+                        <FontAwesomeIcon icon={faHeadphones} />
+                        <FontAwesomeIcon icon={faBook} />
+                        <FontAwesomeIcon icon={faVideo} />
+                        <FontAwesomeIcon icon={faGamepad} />
+                        <FontAwesomeIcon icon={faGlobe} />
+                        <FontAwesomeIcon icon={faComputer} />
+                        <FontAwesomeIcon icon={faDesktop} />
+                        <FontAwesomeIcon icon={faLightbulb} />
+                        <FontAwesomeIcon icon={faPuzzlePiece} />
+                        <FontAwesomeIcon icon={faKeyboard} />
+                        <FontAwesomeIcon icon={faCodeBranch} />
+                        <FontAwesomeIcon icon={faWaterLadder} />
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                        <FontAwesomeIcon icon={faUtensils} />
+                        <FontAwesomeIcon icon={faUserGraduate} />
+                        <FontAwesomeIcon icon={faPodcast} />
+                        <FontAwesomeIcon icon={faPizzaSlice} />
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faFilm} />
+                        <FontAwesomeIcon icon={faHeadphones} />
+                        <FontAwesomeIcon icon={faBook} />
+                        <FontAwesomeIcon icon={faVideo} />
+                        <FontAwesomeIcon icon={faGamepad} />
+                        <FontAwesomeIcon icon={faGlobe} />
+                        <FontAwesomeIcon icon={faComputer} />
+                        <FontAwesomeIcon icon={faDesktop} />
+                        <FontAwesomeIcon icon={faLightbulb} />
+                        <FontAwesomeIcon icon={faPuzzlePiece} />
+                        <FontAwesomeIcon icon={faKeyboard} />
+                        <FontAwesomeIcon icon={faCodeBranch} />
+                        <FontAwesomeIcon icon={faWaterLadder} />
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                        <FontAwesomeIcon icon={faUtensils} />
+                        <FontAwesomeIcon icon={faUserGraduate} />
+                        <FontAwesomeIcon icon={faPodcast} />
+                        <FontAwesomeIcon icon={faPizzaSlice} />
+                    </div>
+                </div>
+            </section>
+
             
             <Contact />
         </>

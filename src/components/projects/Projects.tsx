a7077fc977204a3e5ee5b0fe4c5affd4ba188904
Victor Muniz <victorrmunizz@gmail.com>
2023-12-12 20:10:18 -0300

    create projects screen

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,17 +1,30 @@
 
+import { Background } from '../background/Background';
+import { Nav } from '../nav/Nav';
 import styles from './Projects.module.scss';
 
 export function Projects() {
     return(
         <div className={styles.projectsContainer}>
-            <div>
-                <h1>Em breve!</h1>
-            </div>
-            <div>
-                <h1>Em breve!</h1>
-            </div>
-            <div>
-                <h1>Em breve!</h1>
+            <div className={styles.contentContainer}>
+                <section>
+                    <Background /> 
+                    <Nav />
+                </section>
+                <div>
+                    <h1>Projetos</h1>
+                    <div>
+                        <div>
+                            <h1>Em breve!</h1>
+                        </div>
+                        <div>
+                            <h1>Em breve!</h1>
+                        </div>
+                        <div>
+                            <h1>Em breve!</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
